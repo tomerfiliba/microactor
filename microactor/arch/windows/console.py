4eb41b7179b449c8a2147e2bf8663ad7c663431c
Tomer Filiba <tomerf@il.ibm.com>
2011-04-24 01:25:34 +0300

    console

--- microactor/arch/windows/console.py
+++ microactor/arch/windows/console.py
@@ -1,8 +1,9 @@
 import weakref
+import threading
 import win32api
 import win32console
 import win32event
-from contextlib import contextmanager
+from microactor.utils import ReactorDeferred
 
 
 class Cursor(object):
@@ -90,7 +91,9 @@ class Screen(object):
 
 
 class Console(object):
-    def __init__(self):
+    def __init__(self, reactor):
+        if not self.is_attached():
+            raise ValueError("process not attached to a console")
         self.inbuf = win32console.PyConsoleScreenBufferType(
             win32api.GetStdHandle(win32api.STD_INPUT_HANDLE))
         self.outbuf = win32console.PyConsoleScreenBufferType(
@@ -98,28 +101,60 @@ class Console(object):
         self.clear_events()
         self.cursor = Cursor(weakref.proxy(self))
         self.screen = Screen(weakref.proxy(self))
+        self.reactor = reactor
+        self._events = []
+        self._event_dfr = None
+        self._active = True
+        self._closed_dfr = ReactorDeferred(self.reactor)
+        self._orig_mode = self.inbuf.GetConsoleMode()
+        self._orig_attrs = self.cursor.get_attributes()
+        self.inbuf.SetConsoleMode(self._orig_mode & 
+            ~win32console.ENABLE_LINE_INPUT & 
+            ~win32console.ENABLE_ECHO_INPUT & 
+            ~win32console.ENABLE_PROCESSED_INPUT)
+        self._thd = threading.Thread(name="ConsoleInputThread", target = self._input_watcher_thread)
+        self._thd.start()
     
-    @contextmanager
-    def raw_mode(self):
-        mode = self.inbuf.GetConsoleMode()
-        mode2 = (mode & ~win32console.ENABLE_LINE_INPUT & 
-            ~win32console.ENABLE_ECHO_INPUT & ~win32console.ENABLE_PROCESSED_INPUT)
-        self.inbuf.SetConsoleMode(mode2)
-        attrs = self.cursor.get_attributes()
-        try:
-            yield
-        finally:
-            self.inbuf.SetConsoleMode(mode)
-            self.cursor.set_attributes(attrs)
+    def __enter__(self):
+        return self
+    def __exit__(self, t, v, tb):
+        self.close()
+    def __del__(self):
+        self.close()
     
-    def get_events(self):
+    def close(self):
+        if not self._active:
+            self._active = False
+            self.inbuf.SetConsoleMode(self._orig_mode)
+            self.cursor.set_attributes(self._orig_attrs)
+        return self._closed_dfr
+    
+    @classmethod
+    def allocate(cls):
+        win32console.AllocConsole()
+    
+    @classmethod
+    def is_attached(cls):
+        return win32console.GetConsoleWindow() != 0
+       
+    def _get_events(self):
         return self.inbuf.ReadConsoleInput(100)
-    def get_num_of_events(self):
+    def _get_num_of_events(self):
         return self.inbuf.GetNumberOfConsoleInputEvents()
     def clear_events(self):
         self.inbuf.FlushConsoleInputBuffer()
-    def read(self):
-        return self.inbuf.ReadConsole(100)
+    
+    def read_event(self):
+        if self._event_dfr:
+            raise ValueError("overlapping read")
+        event_dfr = ReactorDeferred(self.reactor)
+        if self._events:
+            evt = self._events.pop(0)
+            event_dfr.set(evt)
+        else:
+            self._event_dfr = event_dfr
+        return event_dfr
+    
     def write(self, text, pos = None, fg = None, bg = None):
         if pos:
             x, y = pos
@@ -128,11 +163,30 @@ class Console(object):
             self.cursor.set_color(fg, bg)
         self.outbuf.WriteConsole(text)
     
+    def on_input(self):
+        if self._get_num_of_events() <= 0:
+            return
+        self._events.extend(self._get_events())
+        if self._event_dfr and not self._event_dfr.is_set():
+            evt = self._events.pop(0) if self.events else None
+            self._event_dfr.set(evt)
+            self._event_dfr = None
+    
+    def _input_watcher_thread(self):
+        try:
+            while self._active:
+                rc = win32event.WaitForSingleObject(self.inbuf, 200) # 0.2 sec
+                if rc == win32event.WAIT_OBJECT_0:
+                    self.reactor.call(self.on_input)
+                    self.reactor._wakeup()
+        finally:
+            self._closed_dfr.set()
+
 
 
 if __name__ == "__main__":
-    con = Console()
-   
+    con = Console(None)
+    
     with con.raw_mode():
         con.screen.clear(bg="gray")
         con.write("hello world\n", (10, 5), fg = "bright_red")
