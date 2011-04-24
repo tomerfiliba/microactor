b9813d63b0b6ae2839c4fb9116748b7662d0ba85
Tomer Filiba <tomerf@il.ibm.com>
2011-04-25 02:12:20 +0300

    udp

--- microactor/arch/windows/console.py
+++ microactor/arch/windows/console.py
@@ -1,12 +1,10 @@
-import weakref
-import threading
-import win32api
 import win32console
 import win32event
-from microactor.utils import ReactorDeferred
+from contextlib import contextmanager
 
 
 class Cursor(object):
+    __slots__ = ["outbuf"]
     COLOR_NAMES = dict(
         black = 0,
         blue = 1,
@@ -28,20 +26,20 @@ class Cursor(object):
     REV_COLOR_NAMES = dict((v, k) for k, v in COLOR_NAMES.items())
     COLOR_NAMES["grey"] = COLOR_NAMES["gray"]
     
-    def __init__(self, con):
-        self.con = con
+    def __init__(self, outbuf):
+        self.outbuf = outbuf
     def get_position(self):
-        info = self.con.outbuf.GetConsoleScreenBufferInfo()
+        info = self.outbuf.GetConsoleScreenBufferInfo()
         cp = info["CursorPosition"]
         return (cp.X, cp.Y)
     def move(self, x, y):
-        self.con.outbuf.SetConsoleCursorPosition(win32console.PyCOORDType(x,y))
+        self.outbuf.SetConsoleCursorPosition(win32console.PyCOORDType(x, y))
     def show(self):
-        size, _ = self.con.outbuf.GetConsoleCursorInfo()
-        self.con.outbuf.SetConsoleCursorInfo(size, True)
+        size, _ = self.outbuf.GetConsoleCursorInfo()
+        self.outbuf.SetConsoleCursorInfo(size, True)
     def hide(self):
-        size, _ = self.con.outbuf.GetConsoleCursorInfo()
-        self.con.outbuf.SetConsoleCursorInfo(size, False)
+        size, _ = self.outbuf.GetConsoleCursorInfo()
+        self.outbuf.SetConsoleCursorInfo(size, False)
     def get_color(self):
         flags = self.get_attributes()
         fg = flags & 0xf
@@ -58,30 +56,37 @@ class Cursor(object):
         flags = self.get_attributes()
         self.set_attributes((flags & ~0xff) | fg | bg)
     def set_attributes(self, flags):
-        self.con.outbuf.SetConsoleTextAttribute(flags)
+        self.outbuf.SetConsoleTextAttribute(flags)
     def get_attributes(self):
-        info = self.con.outbuf.GetConsoleScreenBufferInfo()
+        info = self.outbuf.GetConsoleScreenBufferInfo()
         return info["Attributes"]
+    def write(self, text, pos = (), fg = None, bg = None):
+        if fg or bg:
+            self.set_color(fg, bg)
+        if pos:
+            self.move(*pos)
+        return self.outbuf.WriteConsole(text)
 
 class Screen(object):
-    def __init__(self, con):
-        self.con = con
+    __slots__ = ["outbuf"]
+    def __init__(self, outbuf):
+        self.outbuf = outbuf
     def get_virtual_size(self):
-        info = self.con.outbuf.GetConsoleScreenBufferInfo()
+        info = self.outbuf.GetConsoleScreenBufferInfo()
         s = info["Size"]
         return (s.X, s.Y)
     def get_size(self):
-        info = self.con.outbuf.GetConsoleScreenBufferInfo()
+        info = self.outbuf.GetConsoleScreenBufferInfo()
         s = info["MaximumWindowSize"]
         return (s.X, s.Y)
     def clear(self, fg = "white", bg = "black"):
         x, y = self.get_size()
-        self.con.outbuf.FillConsoleOutputCharacter(u" ", x * y,
+        self.outbuf.FillConsoleOutputCharacter(u" ", x * y,
             win32console.PyCOORDType(0, 0))
         flags = Cursor.COLOR_NAMES[fg] | (Cursor.COLOR_NAMES[bg] << 4) 
-        self.con.outbuf.FillConsoleOutputAttribute(flags, x * y, 
+        self.outbuf.FillConsoleOutputAttribute(flags, x * y, 
             win32console.PyCOORDType(0, 0))
-        self.con.cursor.move(0, 0)
+        self.outbuf.SetConsoleCursorPosition(win32console.PyCOORDType(0, 0))
     def scroll(self, num_of_lines):
         raise NotImplementedError()
     def set_title(self, text):
@@ -89,45 +94,44 @@ class Screen(object):
     def get_title(self):
         return win32console.GetConsoleTitle()
 
+class ConsoleError(OSError):
+    pass
 
 class Console(object):
-    def __init__(self, reactor):
+    def __init__(self):
         if not self.is_attached():
-            raise ValueError("process not attached to a console")
+            raise ConsoleError("process not attached to a console")
         self.inbuf = win32console.PyConsoleScreenBufferType(
-            win32api.GetStdHandle(win32api.STD_INPUT_HANDLE))
+            win32console.GetStdHandle(win32console.STD_INPUT_HANDLE))
         self.outbuf = win32console.PyConsoleScreenBufferType(
-            win32api.GetStdHandle(win32api.STD_OUTPUT_HANDLE))
+            win32console.GetStdHandle(win32console.STD_OUTPUT_HANDLE))
         self.clear_events()
-        self.cursor = Cursor(weakref.proxy(self))
-        self.screen = Screen(weakref.proxy(self))
-        self.reactor = reactor
-        self._events = []
-        self._event_dfr = None
-        self._active = True
-        self._closed_dfr = ReactorDeferred(self.reactor)
-        self._orig_mode = self.inbuf.GetConsoleMode()
-        self._orig_attrs = self.cursor.get_attributes()
-        self.inbuf.SetConsoleMode(self._orig_mode & 
+        self.cursor = Cursor(self.outbuf)
+        self.screen = Screen(self.outbuf)
+    
+    def fileno(self):
+        raise ConsoleError("console doesn't have a fileno")
+    def close(self):
+        if self.inbuf:
+            self.inbuf.close()
+            self.inbuf = None
+        if self.outbuf:
+            self.outbuf.close()
+            self.outbuf = None
+    
+    @contextmanager
+    def raw_mode(self):
+        orig_mode = self.inbuf.GetConsoleMode()
+        orig_attrs = self.cursor.get_attributes()
+        self.inbuf.SetConsoleMode(orig_mode & 
             ~win32console.ENABLE_LINE_INPUT & 
             ~win32console.ENABLE_ECHO_INPUT & 
             ~win32console.ENABLE_PROCESSED_INPUT)
-        self._thd = threading.Thread(name="ConsoleInputThread", target = self._input_watcher_thread)
-        self._thd.start()
-    
-    def __enter__(self):
-        return self
-    def __exit__(self, t, v, tb):
-        self.close()
-    def __del__(self):
-        self.close()
-    
-    def close(self):
-        if not self._active:
-            self._active = False
-            self.inbuf.SetConsoleMode(self._orig_mode)
-            self.cursor.set_attributes(self._orig_attrs)
-        return self._closed_dfr
+        try:
+            yield
+        finally:
+            self.inbuf.SetConsoleMode(orig_mode)
+            self.cursor.set_attributes(orig_attrs)            
     
     @classmethod
     def allocate(cls):
@@ -137,64 +141,32 @@ class Console(object):
     def is_attached(cls):
         return win32console.GetConsoleWindow() != 0
        
-    def _get_events(self):
+    def get_events(self):
         return self.inbuf.ReadConsoleInput(100)
-    def _get_num_of_events(self):
+    def get_num_of_events(self):
         return self.inbuf.GetNumberOfConsoleInputEvents()
     def clear_events(self):
         self.inbuf.FlushConsoleInputBuffer()
-    
-    def read_event(self):
-        if self._event_dfr:
-            raise ValueError("overlapping read")
-        event_dfr = ReactorDeferred(self.reactor)
-        if self._events:
-            evt = self._events.pop(0)
-            event_dfr.set(evt)
+    def read(self, count):
+        return self.inbuf.ReadConsole(count)
+    def write(self, text):
+        return self.outbuf.WriteConsole(text)
+    def wait_input(self, timeout = None):
+        if timeout is None or timeout < 0:
+            timeout = -1
         else:
-            self._event_dfr = event_dfr
-        return event_dfr
-    
-    def write(self, text, pos = None, fg = None, bg = None):
-        if pos:
-            x, y = pos
-            self.cursor.move(x, y)
-        if fg or bg:
-            self.cursor.set_color(fg, bg)
-        self.outbuf.WriteConsole(text)
-    
-    def on_input(self):
-        if self._get_num_of_events() <= 0:
-            return
-        self._events.extend(self._get_events())
-        if self._event_dfr and not self._event_dfr.is_set():
-            evt = self._events.pop(0) if self.events else None
-            self._event_dfr.set(evt)
-            self._event_dfr = None
-    
-    def _input_watcher_thread(self):
-        try:
-            while self._active:
-                rc = win32event.WaitForSingleObject(self.inbuf, 200) # 0.2 sec
-                if rc == win32event.WAIT_OBJECT_0:
-                    self.reactor.call(self.on_input)
-                    self.reactor._wakeup()
-        finally:
-            self._closed_dfr.set()
-
+            timeout = int(timeout * 1000)
+        rc = win32event.WaitForSingleObject(self.inbuf, timeout)
+        return rc == win32event.WAIT_OBJECT_0
 
 
 if __name__ == "__main__":
-    con = Console(None)
-    
+    con = Console()
     with con.raw_mode():
-        con.screen.clear(bg="gray")
-        con.write("hello world\n", (10, 5), fg = "bright_red")
-        print win32event.WaitForSingleObject(con.inbuf, 10000)
+        import os, sys
+        ch = os.read(sys.stdin.fileno(), 1)
+        print "got", ch
 
-        for i in range(10):
-            e = con.read()
-            con.write(repr(e) + "\n", fg = "white")
 
 
 
--- microactor/arch/windows/datagram.py
+++ microactor/arch/windows/datagram.py
@@ -5,8 +5,11 @@ references:
  * http://twistedmatrix.com/trac/browser/trunk/twisted/internet/iocpreactor/iocpsupport/wsarecv.pxi
  * http://www.google.com/codesearch/p?hl=en#T4zviHTmECg/trunk/exeLearning/twisted/internet/iocpreactor/_iocp.c&q=WSASendTo%20lang:python&d=5&l=348
 """
+import win32api
+import win32file
 import ctypes
-import socket # to initialize winsock
+from ctypes import wintypes
+import socket # to make sure winsock is initialized
 
 try:
     winsockdll = ctypes.WinDLL("Ws2_32.dll")
@@ -14,9 +17,97 @@ except WindowsError as ex:
     raise ImportError(str(ex))
 
 
-def WSASendTo():
-    raise NotImplementedError()
+class OVERLAPPED(ctypes.Structure):
+    _fields_ = [('Internal', ctypes.c_void_p),      # ULONG_PTR
+                ('InternalHigh', ctypes.c_void_p),  # ULONG_PTR
+                ('Offset', wintypes.DWORD),         # DWORD
+                ('OffsetHight', wintypes.DWORD),    # DWORD
+                ('hEvent', wintypes.HANDLE),        # HANDLE
+                ]
 
-def WSARecvFrom():
-    raise NotImplementedError()
+class WSABUF(ctypes.Structure):
+    _fields_ = [('len', ctypes.c_ulong),            # u_long
+                ('buf', ctypes.POINTER(ctypes.c_char)),           # char FAR *
+                ]
+
+class SockAddrIP4(ctypes.Structure):
+    _fields_ = [('sin_family', ctypes.c_short),     # short
+                ('sin_port', ctypes.c_ushort),      # u_short
+                ('sin_addr', ctypes.c_ulong),       # struct  in_addr
+                ('sin_zero', ctypes.c_char * 8),    # sin_zero[8]
+                ]
+
+_WSASendTo = winsockdll.WSASendTo
+_WSASendTo.argtypes = [
+    wintypes.HANDLE,                # SOCKET s,
+    ctypes.POINTER(WSABUF),         # LPWSABUF lpBuffers,
+    wintypes.DWORD,                 # DWORD dwBufferCount -- must be 1
+    ctypes.POINTER(wintypes.DWORD), # OUT LPDWORD lpNumberOfBytesSent -- must be NULL
+    wintypes.DWORD,                 # DWORD dwFlags
+    ctypes.POINTER(SockAddrIP4),    # IN struct sockaddr *lpTo
+    ctypes.c_int,                   # int iToLen
+    ctypes.POINTER(OVERLAPPED),     # IN LPWSAOVERLAPPED lpOverlapped
+    wintypes.DWORD,                 # IN LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine -- must be NULL
+] 
+_WSASendTo.restype = ctypes.c_int   # 0 means success
+
+def WSASendTo(hsock, data, addr, overlapped, dwFlags = 0):
+    buf = WSABUF(len(data), data)
+    host, port = addr
+    sockaddr = SockAddrIP4()
+    sockaddr.sin_family = socket.AF_INET
+    sockaddr.sin_port = port
+    sockaddr.sin_addr = socket.inet_aton(host)
+    
+    rc = _WSASendTo(hsock, ctypes.byref(buf), 1, 0, dwFlags, 
+        ctypes.byref(sockaddr), ctypes.sizeof(sockaddr), ctypes.byref(overlapped), 0)
+    if rc != 0:
+        raise ctypes.WinError()
+
+
+_WSARecvFrom = winsockdll.WSARecvFrom
+_WSARecvFrom.argtypes = [
+    wintypes.HANDLE,                # SOCKET s,
+    ctypes.POINTER(WSABUF),         # LPWSABUF lpBuffers,
+    wintypes.DWORD,                 # DWORD dwBufferCount -- must be 1
+    ctypes.POINTER(wintypes.DWORD), # OUT LPDWORD lpNumberOfBytesSent -- must be NULL
+    ctypes.POINTER(wintypes.DWORD), # INOUT LPDWORD lpFlags
+    ctypes.POINTER(SockAddrIP4),    # OUT struct sockaddr *lpFrom
+    ctypes.POINTER(ctypes.c_int),   # INOUT LPINT lpFromlen
+    ctypes.POINTER(OVERLAPPED),     # IN LPWSAOVERLAPPED lpOverlapped
+    wintypes.DWORD,                 # IN LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine -- must be NULL
+]
+_WSARecvFrom.restype = ctypes.c_int # 0 means success 
+
+def WSARecvFrom(hsock, count, overlapped, dwFlags = 0):
+    data = ctypes.create_string_buffer(count)
+    buf = WSABUF(count, data)
+    sockaddr = SockAddrIP4(socket.AF_INET)
+    sockaddr_len = ctypes.c_int(ctypes.sizeof(sockaddr))
+    flags = wintypes.DWORD(dwFlags)
+    sentcount = wintypes.DWORD(0)
+    
+    rc = _WSARecvFrom(hsock, ctypes.byref(buf), 1, ctypes.byref(sentcount),
+        ctypes.byref(flags), ctypes.byref(sockaddr), ctypes.byref(sockaddr_len),
+        ctypes.byref(overlapped), 0)
+    error = win32api.GetLastError()
+    if rc != 0 and error != win32file.WSA_IO_PENDING:
+        raise ctypes.WinError(error)
+    return data, sockaddr
+
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(("0.0.0.0", 12345))
+    from iocp import IOCP
+    ov = win32file.OVERLAPPED()
+    #ov = OVERLAPPED()
+    port = IOCP()
+    port.register(s.fileno())
+    data, sockaddr = WSARecvFrom(s.fileno(), 1000, ov)
+    print port.wait_event(10)
+
+
+
 
